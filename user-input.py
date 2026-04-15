51f9dc10088755aba24da117c81d6f62c80f545f
safin <mahfuzsafin@gmail.com>
2026-04-15 19:30:47 +0600

    user-input

---
+++ user-input.py
@@ -0,0 +1,15 @@
+# user er theke input neoyar jonno input() function call korte hobe
+userName = input("enter your username:")
+password = input("enter your password:")
+print(userName)
+
+num = input("enter your number:")
+print(num + num)
+num1 = int(input("enter your number:"))
+print(num1 + num1)
+
+age = int(input("enter your age:"))
+if age >= 18:
+    print("you are an adult")
+else:
+    print("you are not an adult")
