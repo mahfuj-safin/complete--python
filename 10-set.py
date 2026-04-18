d02bdaab4b57762b87d72da23c3a8299b7f52369
safin <mahfuzsafin@gmail.com>
2026-04-18 16:27:03 +0600

    Access Set

---
+++ 10-set.py
@@ -0,0 +1,21 @@
+#set
+# set is unchangeable
+# set kerlibasis er moddhe likhte hoy
+# set is unorder
+# set duplicate kora jay na
+# set a len() function support kore
+
+
+mySet = {1, False, "Safin",5,6,9}
+print(mySet)
+print(type(mySet))
+print(len(mySet))
+
+
+#Access Set Items
+#for loop er maddhome
+for i in mySet:
+    print(i)
+
+print(10 in mySet)
+
